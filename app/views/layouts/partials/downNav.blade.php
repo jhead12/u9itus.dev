7e0298d6372c38b221adcc92b8d576cb955561bf
Josh <josh.head12@gmail.com>
2015-01-22 17:54:52 -0800

    'csrf:filter3'

--- app/views/layouts/partials/downNav.blade.php
+++ app/views/layouts/partials/downNav.blade.php
@@ -18,7 +18,7 @@
       <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Login <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Registration(<span style="color:red">Unavailable</span> )</a></li>
+              <li><a href="https://dialer.dial4dough.com/affiliates/signup.php#SignupForm">Registration</a></li>
               <li class="divider"></li>
               <li><a href="https://dialer.dial4dough.com/affiliates/login.php#login">Member Login</a></li>
           </ul>
--- app/views/pages/down/index.blade.php
+++ app/views/pages/down/index.blade.php
@@ -9,12 +9,13 @@
       <div class="vert-text ">
         <div >
             
-            <div class="alert alert-danger" style="padding-top:70px;"> We are repairing and updating the AdDials application. We will be back soon. Feel free to contact us if you have any questions or contributions. Thank you.  </div>
         <img src="{{asset('images/off.images/logo.png')}}">
    
         <h2>Get paid up to $1.75 reviewing advertisements, or what we call AdDials</h2>
       
         <p>Advertisers look for loyalty, Dial4dough pays you for your loyalty.</p>
+            <p>
+                <link rel="stylesheet" href="https://dialer.dial4dough.com/affiliates/signup.php#SignupForm" class="btn btn-lg btn-primary"/></p>
         </div>
        
        
