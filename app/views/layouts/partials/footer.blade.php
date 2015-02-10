e739b5c49ff2b6635423eaabff1177f23ce62092
Josh Head <joshhead@joshs-mbp.home>
2015-02-09 18:27:22 -0800

    footer year

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -18,7 +18,7 @@
         <a href="https://twitter.com/dial4dough"><i class="fa fa-twitter fa-2x"></i></a>
 
     </p>
-    <p>&copy; 2014 HeadEnterprise, Inc. &middot; <a href="{{URL::route('privatepolicy')}}">Privacy</a> &middot;<a href="https://blog.dial4dough.com">Blog</a> &middot; <a href="{{URL::route('terms')}}" target="_blank">Terms of
+    <p>&copy; 2015 HeadEnterprise, Inc. &middot; <a href="{{URL::route('privatepolicy')}}">Privacy</a> &middot;<a href="https://blog.dial4dough.com">Blog</a> &middot; <a href="{{URL::route('terms')}}" target="_blank">Terms of
             Service</a></p>
 
     @include('pages.scripts.create')
