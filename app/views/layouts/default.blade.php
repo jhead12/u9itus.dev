06d235dbf2543096ac73f21d7f63fdb15cc99dad
jhead12 <jhead12@fullsail.edu>
2015-04-07 12:32:25 -0700

    meta:added

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,11 +3,14 @@
 
 <head>
 	<meta charset="UTF-8">
+	<meta name="google-site-verification" content="OzhSWDz72APDh5zTIpCvWki0bnXbTUh3SroT0_2UQ-s" />
 	<title>Dial4dough</title>
 
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="{{asset('css/main.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/flashblock.css')}}" />
+
+
 
 	<!-- special IE-only canvas fix -->
 	<!--[if IE]><script type="text/javascript" src="js/excanvas.js"></script><![endif]-->
