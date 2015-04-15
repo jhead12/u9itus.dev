b321af7b2f8e548c740f71b6b7b809178b6153ce
jhead12 <jhead12@fullsail.edu>
2015-04-15 16:51:37 -0700

    paypal add

--- app/views/pages/upgrade.blade.php
+++ app/views/pages/upgrade.blade.php
@@ -17,6 +17,11 @@
 <p dir="ltr" style="line-height: 1.15; margin-top: 0pt; margin-bottom: 0pt;"><b style="font-weight: normal;"><span style="color: rgb(0, 0, 0); font-family: Arial; font-size: 16px; font-style: normal; font-variant: normal; font-weight: bold; text-decoration: none; vertical-align: baseline; background-color: transparent;">Platinum Upgrade</span><span style="color: rgb(0, 0, 0); font-family: Arial; font-size: 16px; font-style: normal; font-variant: normal; font-weight: normal; text-decoration: none; vertical-align: baseline; background-color: transparent;"> - Members can upgrade for one year for $100.00. Members in this class will receive $1.75 for each AdDial completed.</span></b></p>
 
 <p></p>
+        <p><strong>Silver Upgrade</strong> - Members can upgrade for one quarter for $25.00. Members in this class will receive $.75 for each AdDial completed.</p>
+
+        <p>Silver Members sponsoring other members into the Dial4dough program will now have an opportunity to receive $5.00 quarterly (every three months) residual income for each Silver member sponsored; $10.00 yearly for each Gold member sponsored, and $15.00 for each Platinum member sponsored.</p>
+
+        <p>It should be noted that revenue received from recruiting Dial4dough members is residual income.  Each quarter or year as the case may be, that a Platinum member or Gold member reconfirms his or her membership, their qualified sponsor will receive the upgrade bonus in the same amount as the initial recruitment bonus.</p>
 
 <p></p>
 
@@ -58,6 +63,16 @@
                 <input type="hidden" name="custom" value="" id="pap_dx8vc2s5">
                 <script id="pap_x2s6df8d" src="http://dialer.dial4dough.com/scripts/notifysale.php?accountId=default1" type="text/javascript">
                 </script>
+            </form>
+
+        </div>
+        <div class="row">
+            <h3>Silver upgrade</h3>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="6QAFFXPFLTBVY">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
             </form>
 
         </div>
