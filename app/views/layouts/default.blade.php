e7892359f457da6acf26c5f610434971a44713b9
Josh Head <joshhead@JoshsMucMacBook.fios-router.home>
2015-05-06 21:28:17 -0700

    update;

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -31,11 +31,7 @@
 
 	@yield('content')
 
-	   
 
-
-
-</body>
 <script>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
@@ -43,4 +39,26 @@
         js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=296623837209769";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+    var sc_project=10428515;
+    var sc_invisible=1;
+    var sc_security="98419ed9";
+    var scJsHost = (("https:" == document.location.protocol) ?
+            "https://secure." : "http://www.");
+    document.write("<sc"+"ript type='text/javascript' src='" +
+    scJsHost+
+    "statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="web analytics"
+                                      href="http://statcounter.com/" target="_blank"><img
+                    class="statcounter"
+                    src="http://c.statcounter.com/10428515/0/98419ed9/1/"
+                    alt="web analytics"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
+
+</body>
 </html>
