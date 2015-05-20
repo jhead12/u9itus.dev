3e7f90b78719f1a62fbc614302cb84e458d2be51
Josh <josh.head12@gmail.com>
2015-05-19 18:47:00 -0700

    'frontMessage'

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -27,18 +27,6 @@
       </ul>
   </section>
 
-{{--<div id="top" class="header large-10">--}}
-    {{--<div class="vert-text ">--}}
-
-      {{--<!-- Main component for a primary marketing message or call to action -->--}}
-      {{--<div class="jumbotron">--}}
-        {{--<h1>Get paid up to $1.75 reviewing advertisements, or what we call AdDials</h1>--}}
-      {{----}}
-        {{--<p>Advertisers look for loyalty, Dial4dough pays you for you loyalty.</p>--}}
-        {{--<p>--}}
-          {{--<a href="https://dialer.dial4dough.com/affiliates/signup.php#SignupForm" class="'btn btn-lg btn-primary ">Sign Up</a>--}}
-        {{--</p>--}}
-      {{--</div>--}}
 
       @include('pages.partials.content')
 
