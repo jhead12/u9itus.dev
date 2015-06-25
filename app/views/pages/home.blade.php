6615c2e6fc38c3bd27d085cd1a2b020d6273df64
jhead <jhead12@fullsail.edu>
2015-06-25 12:59:21 -0700

    front page:language

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -20,7 +20,7 @@
         </p>
 
 
-      <p>Make .25 cents - $1.75 to listen to Advertisements!</p>
+      <p style="font-size: larger">Earn as much as $1.75 to Listen to  2 minute infomercials!</p>
       <ul class="actions ">
           <li><a href="https://dialer.dial4dough.com/affiliates/signup.php#SignupForm" class="button special">Sign Up</a></li>
           <li><a href="{{URL::to('about')}}" class="button">Learn More</a></li>
