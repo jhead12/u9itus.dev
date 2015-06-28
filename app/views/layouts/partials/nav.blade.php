cdf1d3413b1fa747edbfdf2a4e2c9fc57e01384e
jhead <jhead12@fullsail.edu>
2015-06-28 03:53:12 -0700

    fb

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -8,7 +8,7 @@
 
     <nav id="nav">
         <ul>
-            <li><div class="fb-like"  data-href="http://www.dial4dough.com" data-layout="standard" data-action="like" data-show-faces="false" data-share="false"></div></li>
+            <div class="fb-like"  data-href="http://www.dial4dough.com" data-layout="standard" data-action="like" data-show-faces="false" data-share="false"></div>
 
             <li><a href="{{URL::to('/')}}">Home</a></li>
 
