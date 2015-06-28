7a7b773c366c8a1aeaa46ed566aa079190db80a5
jhead <jhead12@fullsail.edu>
2015-06-28 00:40:19 -0700

    image compression

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -14,8 +14,7 @@
     <meta property="article:section" content="Money">
     <title>Dial4dough</title>
 
-    {{--<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">--}}
-	{{--<link rel="stylesheet" href="{{asset('css/main.css')}}">--}}
+
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
     {{--<link rel="stylesheet" type="text/css" href="{{asset('css/flashblock.css')}}" />--}}
