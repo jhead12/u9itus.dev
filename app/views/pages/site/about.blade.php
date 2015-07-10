f98534853c94c4218eca1dfa9cdd4548eb0e2bed
jhead <jhead12@fullsail.edu>
2015-07-10 10:40:43 -0700

    about us

--- app/views/pages/site/about.blade.php
+++ app/views/pages/site/about.blade.php
@@ -6,35 +6,61 @@
     <section id="main" class="container">
 
         <header>
-            <h2 style="font-size: 5em">What Is Dial4dough All About?</h2>
+            <h2 style="font-size: 5em">What is Dial4dough?</h2>
         </header>
         <div class="box">
 
 
-    <p><strong>Dial4dough</strong> is designed to enable its users to earn income by completing simple tasks, i.e. listening to telephone advertisements, viewing a Website, or video promotion.
-        We recognize that there is a wage-earners crises.  This crises has come upon us because the need for human labor has decreased over the past few decades.  We must now view jobs from a different angle. Dial4dough believes that it can assist in filling a void that can improve the lives of many.</p>
+            <p>Individuals, business owners and business managers now have access to a system that:</p>
 
-    <p><strong>Human participation</strong> in the labor market is shrinking drastically.  Automation, robotics, artificial-intelligence, and other gadgets and gizmos that can be contrived, are directly related to the decreases in our labor markets.  Whether you are butcher, baker or candlestick maker your job is on the chopping block.  Neither Doctors, Lawyers nor Indian Chiefs have job security in this environment.</p>
+            <ol>
+                <li>Reduces advertising cost;</li>
+                <li>Increases R.O.I in marketing expenses;</li>
+                <li>Pays forward marketing savings to customers in their communities for added consumer loyalty;</li>
+                <li>Enhances brand name recognition;</li>
+                <li>Inserts revenue into the community to enhance product consumption;</li>
+                <li>Provides a bargained for exchange for to all users, and much more.</li>
+            </ol>
 
-    <h3>Many blame it directly on outsourcing.</h3>
 
-    <p>Yes. Higher paying jobs are disappearing as a result of outsourcing.  Still we must peep the underlying reason why outsourcing is so easy.   Jobs that once required special skills and trades can now be performed by less talented workers.  This is a result of the high-tech explosion.  As a result of this, jobs will continue to disappear.  Therefore, we must prepare for the inevitable.</p>
+            <h2>System Coordination (two arms)</h2>
 
-    <blockquote><p>It was once believed that small organizations would benefit from the Internet's massive advertising potential, and the playing field would be leveled somwhat. This belief is nowhere near as strong as it was 10 years ago.</p></blockquote>
+            <p>The Dough System is comprised of two arms. The left arm to this system is <a href="http://www.dial4dough.com">Dial4dough.com</a> program.  This program allows its members to subscribe in four different levels.  Each level, depending upon member selection, receives income for viewing audio and video advertisements from participating advertisers such as yourself.</p>
 
-    <p><strong>Many are finding that promoting a product on the Internet is becoming more difficult and expensive. </strong></p>
+            <p>Thirty (30) second to 2 minute advertisements are presented to Dial4dough members via internet and smart devices.  Members view these advertisements for the entire period selected by the advertiser, and are paid from .25 cents per ad view up to $1.75 per ad view, dependent upon subscription level.  Members are also paid in accordance to the length of the advertisement 30 seconds up to 2 - 3 minutes.</p>
 
-    <p>Social media programs and viral marketing systems and the like, have provided the wherewithal for individuals and small businesses to establish a means of contact and dialog.  What is missing however, is a bargained for exchange that provides all involved with a win, win, win regardless of the  entry level.  Dial4dough addresses this issue.</p>
+            <p>Members viewing advertisements must listen to or view the advertisement for the entire allotted time, and be interactive in responding to requests made by the dial4dough.com system to assure that all advertisements are viewed, and viewed for the allotted time period.</p>
 
-    <p><strong>Wage-earners and consumers alike can now take advantage of a brand new way of making purchases, and promoting products.  </strong></p>
+            <h3>Dial4dough member obligations</h3>
 
-    <p>We must learn to restrain ourselves from buying products without revenue return. We must demand payment before we view, listen to, or click on, any advertisement.  We must use consumer leverage to control financial destiny.</p>
+            <p>Members in the Dial4dough.com program are allowed to view advertisements until the member reaches a $100.00 threshold. Once this is accomplished. Members must make a purchase for a minimum of 20 percent of this $100 earnings to purchase products from the promotions of advertisers (You the business owner,etc.) that have provided advertisements to these Dial4dough.com members.</p>
 
-    <p>Many of us are still employed, but as long as new high-tech devices are being created and used by large corporations, our jobs edge closer to the window of vulnerability.</p>
+            <p>If a member elects not to make the 20 percent purchase, then he or she will no longer be provided with advertisements to view.</p>
 
-    <p><strong>Dial4dough </strong>is responding to the crises by providing a forum for revenue, not only for those seeking a sustained income, but also for individuals and businesses that can see the wisdom in using this system.  Everyone wins with Dial4dough.</p>
+            <p>We have discovered that 97 percent of our dial4dough.com members meet the 20 percent purchase obligations immediately.  Many dial4dough members purchase products in excess of their 20 percent obligation. Most members are aware of how the Dough System works, therefore they are more than willing to scratch our advertisers backs in consideration of our business advertisers participating in the Dough System.</p>
 
-            </div>
+            <h3>Addials Quantum 2.50</h3>
+
+            <p>The Addials Quantum 2.50 system is the right arm to the Dough system. This program provides the purchasing platform for individuals, business owners, and business managers alike to buy audio or video ad views that are viewed exclusively by Dial4dough.com members.</p>
+
+            <p>Marketers desiring to purchase advertising may target their advertising messages to a selected area or group. Businesses may promote brick and mortar stores and shops, etc. located locally.</p>
+
+            <p>The Dough System will insure that Dial4dough members viewing these advertisements will be local, and that there will be no advertisement view purchased by our advertisers (you) that will miss its mark or not be viewed for the specific time period requested. This is our guarantee to our advertisers.</p>
+
+            <h3>Methods</h3>
+
+            <p>Many businesses promoting products, goods, services and even ideas have audio and video advertising scripts available. If not, for a reduced fee, The Dough System will provide referrals to talented people that can produce quality voice overs, and video productions for our advertisers.</p>
+
+            <p>Advertisers that have their ad scripts available for public release can simply pay for their ad views right in our system and upload their ad script files to our servers.  Addials Quantum 2.50 will enter the promotion into circulation at the time and place requested by the advertiser.  Although there are many types of files available for audio and video production, our Addials Quantum 2.50 possess the wherewithal to reformat and truncate most files to work properly within the our system.</p>
+
+            <p>There are a myriad of promotional ideas that can be used by businesses to promote their products through our system.  We have found that the coupon system is very effective for organizations such as grocery stores, restaurants and the like.</p>
+
+            <p>Your participation in our community will provide the ends and means for your community to allow businesses, consumers and wage earners alike, that missing ingredient that allows community growth and long term prosperity for all community members.</p>
+
+            <p>The good thing about our program is all community members are aware of the the economic value of participation.  All community members will realize the value of this process.</p>
+
+
+        </div>
 </section>
 
     @include('layouts.partials.footer')
