a09aaf9ed0b34c60f3bb0e041fde54fb5b22d822
jhead <jhead12@fullsail.edu>
2015-07-10 20:55:00 -0700

    fix

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -15,14 +15,13 @@
             <li><a href="#banner">Home</a></li>
             <li><a href="#testimonials">Testimonials</a></li>
 
-
-            <li>
             <li><a href="https://docs.google.com/document/d/1mAD87P2AcXnm9it9kDgxJmnYOYQ_4XzuH9V1cuCK0_w/pub">Products</a>
-
+            </li>
+            <li>
                 <a href="" class="icon fa-angle-down">Philosophy</a>
                 <ul>
                     <li><a href="/polidream">Politicians Dream</a></li>
-                    </li>
+
 
                     <li><a href="{{URL::to('about')}}">About</a></li>
 
