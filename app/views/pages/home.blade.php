24e4c51044bad633442a471e973396de936cdd16
jhead <jhead12@fullsail.edu>
2015-07-12 16:01:39 -0700

    update

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -20,7 +20,7 @@
         </p>
 
 
-      <h1 style="font-size: xx-large;color: #ffff00">Earn as much as $1.75 to Listen to Advertisements!</h1>
+      <h1 style="font-size: xx-large;color: #ffff00">Earn up to $1.75 viewing infomercials as your promote your personal product in a self-generating sales system!!</h1>
       <ul class="actions ">
           <li><a href="https://dialer.dial4dough.com/affiliates/signup.php#SignupForm" class="button special ">Sign Up Now!</a></li>
           <li><a href="{{URL::to('about')}}" class="button">Learn More</a></li>
