13ba951b16a1670c2fe8f0c0e925ec68ef79f5b4
jhead <jhead12@fullsail.edu>
2015-07-14 23:06:53 -0700

    update

--- app/views/pages/aboutus.blade.php
+++ app/views/pages/aboutus.blade.php
@@ -7,57 +7,24 @@
 <br>
 <article>
 
-<h1>What Is Dial4dough All About?</h1>
-    <p>Dial4dough.com and our recently added Adddough.com are products of Head Enterprises, located in Moreno Valley Califorina. Head Ent. has designed a program where consumers, wage-earners, and business owners, alike can have access to a system that:
-        Reduces advertising cost;
-        Increases R.O.I in marketing expenses;
-        Provides great public relations for businesses in the community;
-        Allows businesses to pay forward marketing savings to customers and wage-earners in their communities, for added consumer loyalty;
-        Provides a revenue generating system for consumers and wage earners;
-        Enhances business brand name recognition;
-        Inserts revenue into the community to enhance product consumption;
-        Provides a bargained for exchange for to all users, and much more.</p>
+    <p>Dial4dough is designed to enable its users to earn income by completing simple tasks, i.e. listening to telephone advertisements, viewing a Website, or video promotion. We recognize that there is a wage-earner' crises.  This crisis has come upon us because the need for human labor has decreased over the past few decades.  We must now view jobs from a different angle.
+        Dial4dough believes that it can assist in filling a void that can improve the lives of many.</p>
 
-    <h2>System Coordination (two arms)</h2>
+    <p>Human participation in the labor market is shrinking drastically.  Automation, robotics, computers, artificial-intelligence, and the like, are directly related to the decreases in our labor markets.  Whether you are butcher, baker or candlestick maker your job is on the chopping block.  Neither Doctors, Lawyers nor Indian Chiefs have job security in this environment.
+        Many blame it directly on <strong>outsourcing</strong>.  Yes. Jobs are disappearing as a result of outsourcing.  Still, we must peep the underlying reason why outsourcing is so easy.   Jobs that once required special skills, and trades can now be performed by less talented workers.  This is a result of the high-tech explosion.  Jobs will continue to disappear.  Therefore, we must prepare for the inevitable.</p>
 
-    <p>The Dough System, as we like to call it, is comprised of two arms. The left arm to this system is the  <a href="http://www.dial4dough.com">Dial4dough.com </a>program.  This program allows its members to subscribe in four different levels. The entry level is completely free.  Four levels receives income for viewing audio and video advertisements from participating advertisers.</p>
+    <p>It was once believed that small organizations could benefit from the Internet's massive advertising potential, and the playing field would be leveled somewhat. This belief is no where near as strong as it was 10 years ago.</p>
 
-    <p>Thirty (30) second to 2 minute advertisements are presented to Dial4dough members for viewing through telephones, internet and smart devices.  Members view these advertisements for the entire period selected by the advertiser, and are paid from .05  cents per ad view up to $1.75 per ad view, dependent upon subscription level.  Members are also paid in accordance to the length of the advertisement that is viewed.</p>
+    <p>Many are finding that promoting a product on the Internet is becoming more difficult and expensive. Social media programs and viral marketing systems and the like, have provided the wherewithal for individuals and small businesses to establish a means of contact and dialog.  What is missing however, is a bargained for exchange that provides all involved with a win, win, win, regardless of the entry level.  Dial4dough addresses these issues.</p>
 
-    <p>Members viewing advertisements must listen to, or view the advertisement for the entire allotted time, and be interactive in responding to requests made by the dial4dough.com system to assure that all advertisements are viewed, and viewed for the alloted time period.</p>
+    <p><strong>Wage-earners</strong> and consumers alike, can now take advantage of a brand new way of making purchases, and promoting products.  We must learn to restrain from buying products without revenue return. We must demand payment before we view, listen to, or click on, any advertisement.  We must use consumer leverage to control our financial destiny.</p>
 
-    <h3>Dial4dough member obligations</h3>
+    <p>Many of us are still employed, but as long as new high-tech devices are being created and used by small and large companies corporations, our jobs edge closer to the window of vulnerability.
+        Dial4dough is responding to the crises by providing a forum for revenue, not only for those seeking a sustained income, but also for individuals and businesses that can see the wisdom in using this system.</p>
 
-    <p>Members in the Dial4dough.com program are allowed to view advertisements until the member recieves $100.00 from the Dial4dough system. Once this is accomplished, members must make a purchase for a minimum of 20 percent of this $100 earnings from products promoted by advertisers purchasing addials from our AQ2.50 system.</p>
+    <p><strong>Everyone wins with Dial4dough.</strong> This is our mission.</p>
 
-    <p>If a member elects not to make the 20 percent purchase, then he or she will no longer be provided with advertisements to view until such time that the 20 percent purchase requirement is fulfilled.</p>
-
-    <p>We have discovered that 97 percent of our dial4dough.com members meet the 20 percent purchase obligations immediately.  Many dial4dough members purchase products in excess of their 20 percent obligation and before the obligation becomes due.</p>
-
-    <p>Most of our members are aware of how the Dough System works, therefore they are more than willing to “scratch our advertisers backs”, in consideration for these advertisers participating in the Dough System.</p>
-
-    <h3>Addials Quantum 2.50</h3>
-
-    <p>The Addials Quantum 2.50 system located at <a href="http://Adddough.com">Adddough.com</a>, is the right arm to the Dough system. This program provides the purchasing platform for individuals, business owners, and business managers alike to buy audio or video ad views that are viewed exclusively by Dial4dough.com members.</p>
-
-    <p>Marketers desiring to purchase advertising may target their advertising messages to a selected areas or groups. Businesses may promote brick and mortar stores and shops, etc. located locally.</p>
-
-    <p>The Dough System will insure that Dial4dough members viewing advertisements will be viewed by members within the locations requested exclusively. All ad views requested will be be placed in rotation at the specific location and time requested by the advertiser.</p>
-
-    <h3>Methods</h3>
-
-    <p>Many businesses promoting products, goods, services and even ideas have audio and video advertising scripts available. If not, The Dough System will provide referrals to talented people that can produce quality voice overs, and video productions for our advertisers.</p>
-
-    <p>Advertisers that have their ad scripts available for public release can simply pay for their ad views right in our system, and upload their ad script files to our servers.  Addials Quantum 2.50 will enter the promotion into circulation at the time and place requested by the advertiser.</p>
-
-    <p>Although there are many types of files available for audio and video production, our Addials Quantum 2.50 possess the wherewithal to reformat and truncate most files to work properly within the our system.</p>
-
-    <p>There are a myriad of promotional ideas that can be used by businesses to promote their products through our system.  We have found that the coupon system is very effective for organizations such as grocery stores, restaurants and the like.</p>
-
-    <p>Your participation in our community will provide the ends and means for your community to allow businesses, consumers and wage earners alike, that missing ingredient that allows community growth and long term prosperity for all community members.</p>
-
-    <p>The good thing about our program is most community members will quickly recognize  the economic value that can be realized by participating in our programs.</p>
-
+    <p>Head Enterprises  347-230-8438</p>
 <div class="large-6">
 <a href="{{URL::to('register')}}"><h1 style="color: red">Sign up today.</h1> </a>
 </div>
