0f5e5f83377bb55e9086a199ed954754feb5ce2e
jhead <jhead12@fullsail.edu>
2015-07-27 04:39:44 -0700

    update

--- app/views/maintenance.php
+++ app/views/maintenance.php
@@ -12,7 +12,9 @@
 <article>
     <h1>Dialpad will be back soon!</h1>
     <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:admin@dial4dough.info">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+<!--        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:admin@dial4dough.info">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>-->
+                <p>Sorry for the inconvenience but we&rsquo;re in the process of moving. If you need to you can always <a href="mailto:admin@dial4dough.info">contact us</a>, otherwise we&rsquo;ll be back online in a few days!</p>
+
         <p>&mdash; Dial4dough.com</p>
     </div>
 </article>
